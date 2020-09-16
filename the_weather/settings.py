24e24d3f21c91723e9e931003873cd85cc814015
ankeshkr <32651031+ankeshkr@users.noreply.github.com>
2020-09-17 03:36:38 +0530

    Update settings.py

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'di4t0t5g+4ynji++$py=6rql89=-1kd%0i78kp!^*l-_8#2g!('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-forecast1234.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
